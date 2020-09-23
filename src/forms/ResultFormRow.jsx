50bf4873001e2d2208d0695b541f54aba4bee687
peaky76 <robertjamespeacock@gmail.com>
2020-09-24 00:47:53 +0100

    ResultFormRow given state

--- src/forms/ResultFormRow.jsx
+++ src/forms/ResultFormRow.jsx
@@ -3,27 +3,54 @@ import React, { Component } from "react";
 class ResultFormRow extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      score1: "",
+      score2: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    const tournament = {
-      name: this.state.name,
-      sport: {
-        id: this.state.sportId,
-      },
+
+    const match = {
+      id: this.props.match.id,
+      score1: this.state.score1,
+      score2: this.state.score2,
     };
-    this.props.handlePost(tournament);
+
+    const request = new Request();
+    request
+      .post("/api/team_matches", match)
+      .then(() => (window.location = "/tournaments/" + this.props.id));
   }
 
   render() {
     return (
-      <form className="result-submission">
+      <form className="result-submission" onSubmit={this.handleSubmit}>
         <label>{this.props.match.team1.name}</label>
-        <input type="number" className="input-inline" min="0"></input>
+        <input
+          type="number"
+          className="input-inline"
+          min="0"
+          name="score1"
+          onChange={this.handleChange}
+        ></input>
         <label>{this.props.match.team2.name}</label>
-        <input type="number" className="input-inline" min="0"></input>
+        <input
+          type="number"
+          className="input-inline"
+          min="0"
+          name="score2"
+          onChange={this.handleChange}
+        ></input>
         <button className="button-submit">Save result</button>
       </form>
     );
